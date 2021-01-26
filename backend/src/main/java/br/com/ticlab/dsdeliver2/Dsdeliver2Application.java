29709df9aed46b6801114a692a27315121ae918d
Erinaldo Sanches Nascimento Erinaldo <erinaldo.sanches@ticlab.com.br>
2021-01-26 10:16:14 -0300

    Project created

---
+++ backend/src/main/java/br/com/ticlab/dsdeliver2/Dsdeliver2Application.java
@@ -0,0 +1,13 @@
+package br.com.ticlab.dsdeliver2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dsdeliver2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dsdeliver2Application.class, args);
+	}
+
+}
